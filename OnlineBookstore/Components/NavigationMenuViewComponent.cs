dad4e41803c24c26206bbaf6983474963f7abc29
kdaniel1 <71717114+kdaniel1@users.noreply.github.com>
2021-03-03 19:54:09 -0700

    Assignment 7

---
+++ OnlineBookstore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.Models;
+
+namespace OnlineBookstore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent 
+    {
+        private IBookRepository repository;
+
+        public NavigationMenuViewComponent (IBookRepository b)
+        {
+            repository = b;
+        }
+       public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            return View(repository.Books
+                .Select(x=> x.Category)
+                .Distinct()
+                .OrderBy(x=> x));
+        }
+    }
+}
